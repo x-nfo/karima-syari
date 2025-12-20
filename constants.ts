090fae3027acf863982ebfdd3912ec5fb5f4d2c5
x-info <dmsadikins@gmail.com>
2025-12-20 18:05:19 +0700

    add new product

--- constants.ts
+++ constants.ts
@@ -4,11 +4,39 @@ import { Product } from './types';
 export const PRODUCTS: Product[] = [
   {
     id: 'p1',
+    name: 'Hayya - Black',
+    category: 'Abaya',
+    price: 749000,
+    // Close up / Texture / Neutral
+    image: '/images/hayya-black-1.jpg',
+    hoverImage: '/images/hayya-black-3.jpg',
+    description: 'Inspired by Hayya - an invitation to move forward this abaya brings together feminity and strength. Modest yet empowering, in every detail.',
+    tags: ['everyday', 'basic', 'grey'],
+    colors: ['#808080', '#ffffff', '#000000', '#d3d3d3'],
+    sizes: ['S', 'M', 'L', 'XL']
+
+  },
+  {
+    id: 'p2',
+    name: 'Hayya - Choco',
+    category: 'Abaya',
+    price: 749000,
+    // Close up / Texture / Neutral
+    image: '/images/hayya-choco-1.jpg',
+    hoverImage: '/images/hayya-choco-2.webp',
+    description: 'Inspired by Hayya - an invitation to move forward this abaya brings together feminity and strength. Modest yet empowering, in every detail.',
+    tags: ['everyday', 'basic', 'grey'],
+    colors: ['#808080', '#ffffff', '#000000', '#d3d3d3'],
+    sizes: ['S', 'M', 'L', 'XL']
+
+  },
+  {
+    id: 'p3',
     name: 'Saliha - Almond',
     category: 'Abaya',
     price: 699000,
     // Elegant Black Abaya / Modest Wear
-    image: '/images/saliha almond.jpg',
+    image: '/images/saliha-almond-1.webp',
     hoverImage: '/images/saliha-almond-2.webp',
     description: 'Saliha - for women who dress with faith and dignitiy. More than modest war, its a reminder of beauty in righteousness. Designed in airy crinkle fabric',
     tags: ['elegant', 'black', 'silk', 'evening'],
@@ -17,44 +45,19 @@ export const PRODUCTS: Product[] = [
     sizes: ['S', 'M', 'L', 'XL']
   },
   {
-    id: 'p2',
-    name: 'Safa - Light Blue',
-    category: 'Abaya Denim',
-    price: 699000,
-    // Neutral Beige/Cream Set
-    image: '/images/safa light blue 1.jpg',
-    hoverImage: '/images/safa light blue 3.webp',
-    description: 'Inspired by Safa - the sacred hill of purity this abaya embodies simplicity and grace. Crafted from plain denim, it reflects strength in modesty and softness in spirit.',
-    tags: ['casual', 'beige', 'linen', 'summer'],
-    colors: ['#d2b48c', '#f5f5dc', '#a0522d'],
-    sizes: ['XS', 'S', 'M', 'L']
-  },
-  {
-    id: 'p3',
-    name: 'Marwah - Navy',
-    category: 'Abaya Denim',
-    price: 699000,
-    // Maroon/Red/Dark mood
-    image: '/images/marwah-navy-1.jpg',
-    hoverImage: '/images/marwah-navy-3.webp',
-    description: 'Inspired by Marwah - the sacred hill of Sa`i this abaya reflects softness and faith. Crafted in denim with subtle patternt. it brings color and elegance to modest wear.',
-    tags: ['hijab', 'modest', 'maroon'],
-    colors: ['#800000', '#000000', '#ffc0cb'],
-    sizes: ['S', 'M', 'L', 'XL']
-  },
-  {
     id: 'p4',
-    name: 'Katta - Tosca',
+    name: 'Saliha - Wood',
     category: 'Abaya',
-    price: 785000,
-    // Clean White/Sage or Minimalist
-    image: '/images/tas karima.jpg',
-    hoverImage: '/images/KRMA8184 copy.webp',
-    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
-    tags: ['prayer', 'travel', 'sage'],
-    colors: ['#556b2f', '#f0f8ff', '#708090'],
+    price: 699000,
+    // Close up / Texture / Neutral
+    image: '/images/saliha-wood-1.jpg',
+    hoverImage: '/images/saliha-wood-2.webp',
+    description: 'Saliha - for women who dress with faith and dignitiy. More than modest war, its a reminder of beauty in righteousness. Designed in airy crinkle fabric',
+    tags: ['everyday', 'basic', 'grey'],
+    colors: ['#808080', '#ffffff', '#000000', '#d3d3d3'],
     sizes: ['S', 'M', 'L', 'XL']
   },
+
   {
     id: 'p5',
     name: 'Katta - Terracotta',
@@ -70,17 +73,211 @@ export const PRODUCTS: Product[] = [
   },
   {
     id: 'p6',
-    name: 'Hayya - Black',
+    name: 'Katta - Dusty Pink',
     category: 'Abaya',
-    price: 749000,
+    price: 699000,
     // Close up / Texture / Neutral
-    image: '/images/KRMA8658 copy.jpg',
-    hoverImage: '/images/KRMA8665 copy.jpg',
-    description: 'Inspired by Hayya - an invitation to move forward this abaya brings together feminity and strength. Modest yet empowering, in every detail.',
+    image: '/images/katta-dusty-pink-1.jpg',
+    hoverImage: '/images/katta-dusty-pink-2.jpg',
+    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
     tags: ['everyday', 'basic', 'grey'],
     colors: ['#808080', '#ffffff', '#000000', '#d3d3d3'],
     sizes: ['S', 'M', 'L', 'XL']
-  }
+  },
+  {
+    id: 'p7',
+    name: 'Katta - Olive',
+    category: 'Abaya',
+    price: 699000,
+    // Close up / Texture / Neutral
+    image: '/images/katta-olive-1.jpg',
+    hoverImage: '/images/katta-olive-2.webp',
+    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
+    tags: ['everyday', 'basic', 'grey'],
+    colors: ['#808080', '#ffffff', '#000000', '#d3d3d3'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p8',
+    name: 'Katta - Moss',
+    category: 'Abaya',
+    price: 699000,
+    // Maroon/Red/Dark mood
+    image: '/images/katta-moss-1.webp',
+    hoverImage: '/images/katta-moss-2.webp',
+    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
+    tags: ['prayer', 'travel', 'sage'],
+    colors: ['#556b2f', '#f0f8ff', '#708090'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+
+  {
+    id: 'p9',
+    name: 'Katta - Sage',
+    category: 'Abaya',
+    price: 785000,
+    // Clean White/Sage or Minimalist
+    image: '/images/katta-sage-1.webp',
+    hoverImage: '/images/katta-sage-2.webp',
+    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
+    tags: ['prayer', 'travel', 'sage'],
+    colors: ['#556b2f', '#f0f8ff', '#708090'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p10',
+    name: 'Katta - Tosca',
+    category: 'Abaya',
+    price: 785000,
+    // Clean White/Sage or Minimalist
+    image: '/images/katta-tosca-1.webp',
+    hoverImage: '/images/katta-tosca-2.webp',
+    description: 'Katta speaks in lines and strokes - a story woven into fabric. Minimal yet expressive, designed in textured striped material for a graceful everyday wear.',
+    tags: ['prayer', 'travel', 'sage'],
+    colors: ['#556b2f', '#f0f8ff', '#708090'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+
+  {
+    id: 'p11',
+    name: 'Safa - Navy',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Neutral Beige/Cream Set
+    image: '/images/safa-navy-2.webp',
+    hoverImage: '/images/safa-navy-3.webp',
+    description: 'Inspired by Safa - the sacred hill of purity this abaya embodies simplicity and grace. Crafted from plain denim, it reflects strength in modesty and softness in spirit.',
+    tags: ['casual', 'beige', 'linen', 'summer'],
+    colors: ['#d2b48c', '#f5f5dc', '#a0522d'],
+    sizes: ['XS', 'S', 'M', 'L']
+
+  },
+  {
+    id: 'p12',
+    name: 'Safa - Jeans',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Neutral Beige/Cream Set
+    image: '/images/safa-jeans-2.jpg',
+    hoverImage: '/images/safa-jeans-3.webp',
+    description: 'Inspired by Safa - the sacred hill of purity this abaya embodies simplicity and grace. Crafted from plain denim, it reflects strength in modesty and softness in spirit.',
+    tags: ['casual', 'beige', 'linen', 'summer'],
+    colors: ['#d2b48c', '#f5f5dc', '#a0522d'],
+    sizes: ['XS', 'S', 'M', 'L']
+
+  },
+  {
+    id: 'p13',
+    name: 'Safa - Blue',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Neutral Beige/Cream Set
+    image: '/images/safa-blue-1.webp',
+    hoverImage: '/images/safa-blue-2.webp',
+    description: 'Inspired by Safa - the sacred hill of purity this abaya embodies simplicity and grace. Crafted from plain denim, it reflects strength in modesty and softness in spirit.',
+    tags: ['casual', 'beige', 'linen', 'summer'],
+    colors: ['#d2b48c', '#f5f5dc', '#a0522d'],
+    sizes: ['XS', 'S', 'M', 'L']
+
+  },
+  {
+    id: 'p14',
+    name: 'Safa - Light Blue',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Neutral Beige/Cream Set
+    image: '/images/safa light blue 1.jpg',
+    hoverImage: '/images/safa light blue 3.webp',
+    description: 'Inspired by Safa - the sacred hill of purity this abaya embodies simplicity and grace. Crafted from plain denim, it reflects strength in modesty and softness in spirit.',
+    tags: ['casual', 'beige', 'linen', 'summer'],
+    colors: ['#d2b48c', '#f5f5dc', '#a0522d'],
+    sizes: ['XS', 'S', 'M', 'L']
+
+  },
+
+
+
+
+  {
+    id: 'p15',
+    name: 'Marwah - Navy',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Maroon/Red/Dark mood
+    image: '/images/marwah-navy-1.jpg',
+    hoverImage: '/images/marwah-navy-3.webp',
+    description: 'Inspired by Marwah - the sacred hill of Sa`i this abaya reflects softness and faith. Crafted in denim with subtle patternt. it brings color and elegance to modest wear.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p16',
+    name: 'Marwah - Jeans',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Maroon/Red/Dark mood
+    image: '/images/marwah-jeans-1.webp',
+    hoverImage: '/images/marwah-jeans-2.webp',
+    description: 'Inspired by Marwah - the sacred hill of Sa`i this abaya reflects softness and faith. Crafted in denim with subtle patternt. it brings color and elegance to modest wear.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p17',
+    name: 'Marwah - Blue',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Maroon/Red/Dark mood
+    image: '/images/marwah-blue-1.webp',
+    hoverImage: '/images/marwah-blue-2.webp',
+    description: 'Inspired by Marwah - the sacred hill of Sa`i this abaya reflects softness and faith. Crafted in denim with subtle patternt. it brings color and elegance to modest wear.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p18',
+    name: 'Marwah - Light Blue',
+    category: 'Abaya Denim',
+    price: 699000,
+    // Maroon/Red/Dark mood
+    image: '/images/marwah-light-blue-1.webp',
+    hoverImage: '/images/marwah-light-blue-3.webp',
+    description: 'Inspired by Marwah - the sacred hill of Sa`i this abaya reflects softness and faith. Crafted in denim with subtle patternt. it brings color and elegance to modest wear.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p19',
+    name: 'Khimar',
+    category: 'Khimar',
+    price: 349000,
+    // Maroon/Red/Dark mood
+    image: '/images/khimar-1.webp',
+    hoverImage: '/images/khimar-1.webp',
+    description: 'Grace in motion. The Side Khimar brings effortless modesty in six timeless shades, perfect for both daily wear and special moments.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+  {
+    id: 'p20',
+    name: 'Khimar Bandana',
+    category: 'Khimar Bandana',
+    price: 349000,
+    // Maroon/Red/Dark mood
+    image: '/images/khimar-bandana-1.webp',
+    hoverImage: '/images/khimar-bandana-1.webp',
+    description: 'The Khimar Bandana offers a chic and modest twist for your daily wear. Available in four easy-to-style colors.',
+    tags: ['hijab', 'modest', 'maroon'],
+    colors: ['#800000', '#000000', '#ffc0cb'],
+    sizes: ['S', 'M', 'L', 'XL']
+  },
+
+
 ];
 
 export const AI_SYSTEM_INSTRUCTION = `
